6b522694c1dd3c0b47b2e214dbec4061064fe1bc
Carlos Castro <carlos.castro@mongodb.com>
2021-12-04 14:32:12 +0000

    day 4 complete;

---
+++ d4/d4p1.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func addRemaining(array [5][5]string) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+
+			if array[j][i] != "-1" {
+				value, err := strconv.Atoi(array[j][i])
+				if err != nil {
+					log.Fatal(err)
+				}
+				sum += value
+			}
+		}
+	}
+	return sum
+}
+
+func findWinningRow(array [5][5]string) int {
+
+	for i := 0; i < 5; i++ {
+		sum := 0
+		for j := 0; j < 5; j++ {
+			value, err := strconv.Atoi(array[i][j])
+			if err != nil {
+				log.Fatal(err)
+			}
+			sum += value
+		}
+		if sum == -5 {
+			fmt.Printf("Found winning row %v\n", i)
+			return i
+		}
+	}
+	return -1
+}
+
+func findWinningColumn(array [5][5]string) int {
+
+	for i := 0; i < 5; i++ {
+		sum := 0
+		for j := 0; j < 5; j++ {
+			value, err := strconv.Atoi(array[j][i])
+			if err != nil {
+				log.Fatal(err)
+			}
+			sum += value
+		}
+		if sum == -5 {
+			fmt.Printf("Found winning column %v\n", i)
+			return i
+		}
+	}
+	return -1
+}
+
+func contains(array [5]string, value string) int {
+
+	for i := 0; i < len(array); i++ {
+		if array[i] == value {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	file, err := os.Open("puzzle_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	line_n := 0
+	board_number := 0
+	board_row := 0
+	var boards [100][5][5]string
+	var draw []string
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line_n == 0 {
+			draw = strings.Split(line, ",")
+		} else if len(line) > 1 {
+			content := strings.Split(line, " ")
+			copy(boards[board_number][board_row][:], content)
+			board_row++
+		} else {
+			board_row = 0
+			if line_n > 2 {
+				board_number++
+			}
+		}
+		line_n++
+	}
+
+	//fmt.Printf("draw is %v\n", draw)
+	//fmt.Printf("boards are %v\n", boards)
+
+	/* 	board := [][]int{
+	   		{14, 21, 17, 24, 4},
+	   		{10, 16, 15, 9, 19},
+	   		{18, 8, 2100, 26, 20},
+	   		{22, 11, 1100, 6, 5},
+	   		{2, 0, 12, 100, 7},
+	   	}
+
+	   	draw := []int{
+	   		7, 4, 9, 5, 11, 17, 2100, 2, 0, 14, 21, 24, 10, 16, 1100, 6, 15, 25, 12, 22, 18, 20, 8, 19, 100, 26, 1,
+	   	}
+	*/
+
+	winning_number := 0
+	winning_board := 0
+	board_index := 0
+	for i := 0; i < len(draw); i++ {
+		for board_index = 0; board_index < 100; board_index++ {
+			for j := 0; j < 5; j++ {
+				fmt.Printf("checking if value %v exists in row %v of the board number %v with content %v\n", draw[i], j, board_index, boards[board_index][j])
+				index := contains(boards[board_index][j], draw[i])
+				if index > -1 {
+					boards[board_index][j][index] = "-1"
+				}
+
+				win_row := findWinningRow(boards[board_index])
+				win_col := findWinningColumn(boards[board_index])
+
+				if win_row > -1 {
+					// BINGO
+					fmt.Printf("Bingo! Winning row is %v\n", win_row)
+					fmt.Printf("Bingo! Remaining numbers are %v\n", boards[board_index])
+
+					winning_number, err = strconv.Atoi(draw[i])
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					fmt.Printf("Winning number is %v\n", winning_number)
+
+					j = 5
+					i = len(draw)
+					winning_board = board_index
+					board_index = 100
+
+				} else if win_col > -1 {
+					// BINGO
+					fmt.Printf("Bingo! Winning column is %v\n", win_col)
+					fmt.Printf("Bingo! Remaining numbers are %v\n", boards[board_index])
+
+					winning_number, err = strconv.Atoi(draw[i])
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					fmt.Printf("Winning number is %v\n", winning_number)
+
+					j = 5
+					i = len(draw)
+					winning_board = board_index
+					board_index = 100
+
+				}
+			}
+		}
+	}
+
+	remaining := addRemaining(boards[winning_board])
+	fmt.Printf("Remaining total is is %v\n", remaining)
+	fmt.Printf("Result is %v\n", remaining*winning_number)
+
+}
